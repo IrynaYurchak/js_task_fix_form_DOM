6e8be50fd1f84b34553eb634e0d255ae94b8b6e5
Iryna <Yurchak.Iryna2017@gmail.com>
2025-09-24 18:18:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach(input => {
+
+  const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  const label = document.createElement('label');
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = labelText;
+
+  input.setAttribute('placeholder', labelText);
+
+  input.parentElement.insertBefore(label, input);
+})
